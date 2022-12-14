8d7516da3317bafd056ff8c3c22192230fc8207e
Dhanach Dev <dhanachintrsuk@gmail.com>
2022-12-14 09:38:49 +0700

    first commit

---
+++ pages/cards/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <Cards />
+</template>
+
+<style>
+* {
+    font-family: 'Prompt';
+    font-weight: 200;
+}
+
+body {
+    
+    background: url(../../assets/img/bottom-seamless.svg) no-repeat bottom;
+    background-size: cover;
+    background-attachment: fixed;
+    background-color: #f1f1f1;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <Home/>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+* {
+  font-family: 'Prompt';
+}
+
+body {
+    /* background: url(../assets/img/bottom-seamless.svg); */
+    background: url(/library/demo/_nuxt/img/bottom-seamless.svg) no-repeat bottom;
+    background-size: cover;
+    background-attachment: fixed;
+    background-color: #f1f1f1;
+}
+
+button {
+  cursor: pointer;
+  background: white;
+}
+</style>
