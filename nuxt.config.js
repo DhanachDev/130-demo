7639946050f36447c80354424dda76192294b298
Dhanach Dev <dhanachintrsuk@gmail.com>
2022-12-14 09:54:43 +0700

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,8 +38,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    // filenames: {
-    //   img: 'img/[name].[ext]'
-    // }
+    filenames: {
+      img: 'img/[name].[ext]'
+    }
   },
+  target: 'static'
 }
